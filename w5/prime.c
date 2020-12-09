070e6825c95eb52dfcc5f1452ce065dfd781bbb4
momoknight <yincheng.tao@outlook.com>
2020-12-09 13:42:46 +0800

    修改判断是否是素数的代码

--- w5/prime.c
+++ w5/prime.c
@@ -7,6 +7,11 @@ int main()
 
     printf("Please enter an integer number: ");
     scanf("%d", &n);
+    if (n == 1)
+    {
+        isPrime = 0;
+    }
+    
     for (int i = 2; i < n; i++) {
         if (n % i == 0) {
             isPrime = 0;
